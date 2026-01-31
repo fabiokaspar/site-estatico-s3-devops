f2c5d521b635de1845a1adb6c11d27ffe28255cf
root <root@Fabio.localdomain>
2026-01-30 21:29:34 -0300

    replace gif on index page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import jim from './jim.gif';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={jim} className="App-logo" alt="logo" />
         <p>
           Curso Terraform com AWS (DevOps)
         </p>
